9724ab5e017805285a9262a4f27892178f1532e3
Shipu8644 <shipu.monjurur@gmail.com>
2021-10-24 03:23:36 +0600

    crd done

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+
+// middleWare
+app.use(cors());
+app.use(express.json());
 
 // user: geniusMechanic
 // password: 99fSkR05VTXzAwiq
@@ -13,16 +19,43 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        console.log('connected to db right');
-        //   const database = client.db("insertDB");
-        //   const haiku = database.collection("haiku");
-        //   // create a document to insert
-        //   const doc = {
-        //     title: "Record of a Shriveled Datum",
-        //     content: "No bytes, no problem. Just insert a document, in MongoDB",
-        //   }
-        //   const result = await haiku.insertOne(doc);
-        //   console.log(`A document was inserted with the _id: ${result.insertedId}`);
+        const database = client.db("carMechanic");
+        const servicesCollection = database.collection("services");
+
+        // get api
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        //get Single Service
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.send(service);
+        })
+
+        //   Post Api
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            const result = await servicesCollection.insertOne(service);
+            console.log(result);
+            res.json(result);
+        })
+
+        //delete api
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.deleteOne(query);
+            res.send(service);
+        })
+
+
+
+
     } finally {
         //   await client.close();
     }
